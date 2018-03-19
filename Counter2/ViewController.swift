fb2d94ea62aa49dd280a36371ee5e2debdde8dd3
D7702_09 <d7702_09@D7702-09ui-iMac.local>
2018-03-19 15:27:33 +0900

    add action outlet var

--- Counter2/ViewController.swift
+++ Counter2/ViewController.swift
@@ -1,20 +1,30 @@
-//
-//  ViewController.swift
-//  Counter2
-//
-//  Created by D7702_09 on 2018. 3. 19..
-//  Copyright © 2018년 lyw. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var count:Int = 0;
+    
+    @IBOutlet weak var LblA: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func BtA(_ sender: Any) {
+        print("BtA #\(count)")
+        count = count + 1
+        LblA.text = String(count)
+    }
+    
+    @IBAction func BtB(_ sender: Any) {
+        count = 0
+        LblA.text = String(count)
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
